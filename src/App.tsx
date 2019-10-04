5b441c897a9a0d29cac1316ea5a63cee98aacceb
Ilia Safronov <vabole@gmail.com>
2019-10-04 18:45:32 +0300

    display fixture list

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { TopBar } from "./components/TopBar";
+import { initialState } from "./models/initialState";
 
+const fixtures = initialState.entities.fixtures;
 export const App = () => {
-  return <TopBar />;
+  return <div>{fixtures && <TopBar fixtures={fixtures} />}</div>;
 };
--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -1,9 +1,22 @@
 import React from "react";
-import { initialState } from "../models/initialState";
 import { FixtureBox } from "./FixtureBox";
+import { FixtureStateObject } from "../models/types";
 
-console.log(initialState);
+type TopBarProps = {
+  fixtures: { [id: string]: FixtureStateObject };
+};
 
-export const TopBar = () => {
-  return <FixtureBox />;
+export const TopBar = (props: TopBarProps) => {
+  const { fixtures } = props;
+  return (
+    <>
+      {Object.values(fixtures).map(fixture => (
+        <FixtureBox
+          id={fixture.id}
+          color={fixture.color || "blue"}
+          key={fixture.id}
+        />
+      ))}
+    </>
+  );
 };
