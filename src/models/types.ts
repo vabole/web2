6b2d6436c814930a71d378ff45e13c45723f276e
Ilia Safronov <vabole@gmail.com>
2019-10-04 18:32:14 +0300

    types

--- src/models/types.ts
+++ src/models/types.ts
@@ -1,61 +1,56 @@
-import { RouterState } from 'connected-react-router';
-import { couponState } from './reducer';
-
 export type SportFull = Omit<
-    Safl.SportTreeElement,
-    'sportId' | 'count' | 'ordinal'
+  Safl.SportTreeElement,
+  "sportId" | "count" | "ordinal"
 > & {
-    id: string;
-    categories: { [id: string]: CategoryFull };
-    count?: number;
-    ordinal?: number;
+  id: string;
+  categories: { [id: string]: CategoryFull };
+  count?: number;
+  ordinal?: number;
 };
 
 export type CategoryFull = Omit<
-    Safl.CategoryTreeElement,
-    'categoryId' | 'count' | 'ordinal'
+  Safl.CategoryTreeElement,
+  "categoryId" | "count" | "ordinal"
 > & {
-    // export type CategoryFull = Safl.CategoryTreeElement & {
-    tournaments: { [id: string]: TournamentFull };
-    id: string;
-    count?: number;
-    ordinal?: number;
+  // export type CategoryFull = Safl.CategoryTreeElement & {
+  tournaments: { [id: string]: TournamentFull };
+  id: string;
+  count?: number;
+  ordinal?: number;
 };
 
 export type TournamentFull = Omit<
-    Safl.TournamentTreeElement,
-    'tournamentId' | 'count' | 'ordinal'
+  Safl.TournamentTreeElement,
+  "tournamentId" | "count" | "ordinal"
 > & {
-    id: string;
-    fixtures: { [id: string]: FixtureFull };
-    count?: number;
-    ordinal?: number;
+  id: string;
+  fixtures: { [id: string]: FixtureFull };
+  count?: number;
+  ordinal?: number;
 };
 
 export type MatchStatus =
-    | 'LIVE'
-    | 'NOT_STARTED'
-    | 'SUSPENDED'
-    | 'ENDED'
-    | 'FINISHED'
-    | 'CANCELLED'
-    | 'ABANDONED'
-    | 'DELAYED'
-    | 'UNKNOWN';
+  | "LIVE"
+  | "NOT_STARTED"
+  | "SUSPENDED"
+  | "ENDED"
+  | "FINISHED"
+  | "CANCELLED"
+  | "ABANDONED"
+  | "DELAYED"
+  | "UNKNOWN";
 
-export type FixtureFull = Omit<Safl.FixtureElement, 'fixtureId'> & {
-    id: string;
-    status: MatchStatus;
+export type FixtureFull = Omit<Safl.FixtureElement, "fixtureId"> & {
+  id: string;
+  status: MatchStatus;
 };
 
 export type SpirsState = {
-    spirs: {
-        fixtures: NormalizedState;
-        playerInfo: Safl.PlayerModel;
-        steps: Safl.StepForm[];
-        coupons: couponState;
-    };
-    router: RouterState;
+  spirs: {
+    fixtures: NormalizedState;
+    playerInfo: Safl.PlayerModel;
+    steps: Safl.StepForm[];
+  };
 };
 
 export type NormalizedState = { entities: StateEntities; result?: number[] };
@@ -66,35 +61,35 @@ export type TournamentsState = { [key: string]: TournamentStateObject };
 export type FixturesState = { [key: string]: FixtureStateObject };
 
 export type StateEntities = {
-    sports: SportState | null;
-    categories: CategoriesState | null;
-    tournaments: TournamentsState | null;
-    fixtures: FixturesState | null;
+  sports: SportState | null;
+  categories: CategoriesState | null;
+  tournaments: TournamentsState | null;
+  fixtures: FixturesState | null;
 };
 
-export type SportStateObject = Omit<SportFull, 'categories'> & {
-    categories: string[];
+export type SportStateObject = Omit<SportFull, "categories"> & {
+  categories: string[];
 };
 
-export type CategoryStateObject = Omit<CategoryFull, 'tournaments'> & {
-    tournaments: string[];
+export type CategoryStateObject = Omit<CategoryFull, "tournaments"> & {
+  tournaments: string[];
 };
 
-export type TournamentStateObject = Omit<TournamentFull, 'fixtures'> & {
-    fixtures: string[];
+export type TournamentStateObject = Omit<TournamentFull, "fixtures"> & {
+  fixtures: string[];
 };
 
 export type FixtureStateObject = FixtureFull;
 
 export type FixtureStatus =
-    | 'NOT_STARTED'
-    | 'LIVE'
-    | 'SUSPENDED'
-    | 'ENDED'
-    | 'FINISHED'
-    | 'CANCELLED'
-    | 'ABANDONED'
-    | 'DELAYED'
-    | 'UNKNOWN';
+  | "NOT_STARTED"
+  | "LIVE"
+  | "SUSPENDED"
+  | "ENDED"
+  | "FINISHED"
+  | "CANCELLED"
+  | "ABANDONED"
+  | "DELAYED"
+  | "UNKNOWN";
 
-export const couponTypeArray = ['ORDINAR', 'EXPRESS', 'SYSTEM'] as const;
+export const couponTypeArray = ["ORDINAR", "EXPRESS", "SYSTEM"] as const;
