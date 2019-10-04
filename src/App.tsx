ba9d53abd2d0a9b54dddf35675e67f75ece6536a
Ilia Safronov <vabole@gmail.com>
2019-10-04 18:32:32 +0300

    blank TopBar

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from "react";
+import { TopBar } from "./components/TopBar";
 
 export const App = () => {
-  return <div />;
+  return <TopBar />;
 };
-
---
+++ src/components/TopBar.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { initialState } from "../models/initialState";
+import { FixtureBox } from "./FixtureBox";
+
+console.log(initialState);
+
+export const TopBar = () => {
+  return <FixtureBox />;
+};
