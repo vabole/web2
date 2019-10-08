9d56ae525c7fd082217765e1216bf0e48779a396
Ilia Safronov <vabole@gmail.com>
2019-10-08 15:22:39 +0300

    Market Styling

--- src/components/MarketView.tsx
+++ src/components/MarketView.tsx
@@ -1,5 +1,29 @@
 import React from "react";
 import { MarketState } from "./marketTypes";
+import styled from "styled-components";
+
+const MarketViewStyled = styled.div`
+  color: #c8c8c8;
+  font-weight: var(--font-bold);
+  font-size: 22px;
+  display: flex;
+  flex-direction: row;
+`;
+const SelectionBox = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  * {
+    background-color: #292a2c;
+    line-height: calc(var(--spacing) * 5);
+    min-width: calc(var(--spacing) * 7);
+    padding: 0 calc(var(--spacing) * 1);
+    margin: 1px;
+  }
+  .odds {
+    color: #42b6f6;
+  }
+`;
 
 type MarketDataProps = {
   data: MarketState;
@@ -11,28 +35,28 @@ export const MarketView = (props: MarketDataProps) => {
   switch (data.type) {
     case "market":
       return (
-        <>
+        <MarketViewStyled>
           {data.label && <div className="marketHeader">{data.label}</div>}
           {data.data.map(marketDataItem => (
             <MarketView data={marketDataItem}></MarketView>
           ))}
-        </>
+        </MarketViewStyled>
       );
     case "selections":
       return (
-        <>
+        <MarketViewStyled>
           {data.label && <div className="marketHeader">{data.label}</div>}
 
           {data.data.map(selectionItem => (
             // <key={selectionItem.id}>
-            <React.Fragment key={selectionItem.content.id}>
+            <SelectionBox key={selectionItem.content.id}>
               {selectionItem.label && (
                 <div className="selectionLabel">{selectionItem.label}</div>
               )}
               <div className="odds">{selectionItem.content.odds}</div>
-            </React.Fragment>
+            </SelectionBox>
           ))}
-        </>
+        </MarketViewStyled>
       );
     default:
       return null;
